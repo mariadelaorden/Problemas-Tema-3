680f53019d171e5df31a3ab6b5d54e1b3f935d52
candelasanzm <sanzmartincandela@gmail.com>
2025-03-07 11:10:53 +0100

    ej4

--- ejercicio4.py
+++ ejercicio4.py
@@ -13,6 +13,7 @@ def comparar (botella : int, corcho : int) -> int:
 
 def taponar_botellas(botellas : list[int], corchos : list[int]) -> bool :
     """ Algoritmo principal para taponar las botellas """
+    # Caso base: si solo hay una botella y un corchos, verifico si el corcho es adecuado para la botella
     if len(botellas) <= 1 :
         if botellas and corchos and tamano_correcto(botellas[0], corchos[0]):
             return [(botellas[0], corchos[0])]
@@ -22,22 +23,40 @@ def taponar_botellas(botellas : list[int], corchos : list[int]) -> bool :
     # Seleccionar una botella de inicio
     botella_inicial = botellas[0]
 
-    # Dividir los corchos en dos grupos: corchos más grandes y corchos más pequeños
+    # Dividir los corchos en dos grupos: corchos grandes, adecuados y pequeños
     corchos_adecuados = []
     corchos_grandes = []
     corchos_pequenos = []
 
+    # Comparo los corchos con las botellas y los asigno a cada lista según corresponda
     for corcho in corchos: 
         comparo = comparar(botella_inicial, corcho)
         if comparo == 0:
             corchos_adecuados.append(corcho)
-        elif comparto < 0:
+        elif comparo < 0:
             corchos_pequenos.append(corcho)
         else:
             corchos_grandes.append(corcho)
 
+    # Si ningún corcho es adecuado, devolverá una lista vacía
     if not corchos_adecuados :
         return []
     
+    # Encuentro el corcho adecuado y lo quito de la lista
     corcho_adecuado = corchos_adecuados[0]
     corchos.remove(corcho_adecuado)
+
+    # Divido la lista a la mitad y recursivamente voy comprobando los corchos
+    mitad_izquierda = taponar_botellas(botellas[1:], corchos_pequenos)
+    mitad_derecha = taponar_botellas(botellas[1:], corchos_grandes)
+
+    # Combino las soluciones
+    mitades = [(botella_inicial, corcho_adecuado)] + mitad_izquierda + mitad_derecha
+
+    return mitades
+
+
+botellas = [1, 2, 3, 4]
+corchos = [2, 4, 1, 3]
+resultado = taponar_botellas(botellas, corchos)
+print (resultado)
